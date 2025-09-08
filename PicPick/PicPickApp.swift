9d6a166fc3e567a780e32d0205c7f8199de6db9c
PatatoBit <pathonscript@gmail.com>
2025-09-08 21:48:33 +0700

    Initial Commit

---
+++ PicPick/PicPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicPickApp.swift
+//  PicPick
+//
+//  Created by Patato on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PicPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PicPickTests/PicPickTests.swift
@@ -0,0 +1,17 @@
+//
+//  PicPickTests.swift
+//  PicPickTests
+//
+//  Created by Patato on 8/9/25.
+//
+
+import Testing
+@testable import PicPick
+
+struct PicPickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
