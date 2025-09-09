36b2526dc7c69d90d68d5d951be1abbaff3d0211
PatatoBit <pathonscript@gmail.com>
2025-09-09 21:30:58 +0700

    image dragging with rotation

--- PicPick/PhotoAreaView.swift
+++ PicPick/PhotoAreaView.swift
@@ -14,9 +14,7 @@ struct PhotoAreaView: View {
       } else {
         ZStack {
           ForEach(images.indices, id: \.self) { index in
-            Image(uiImage: images[index])
-              .resizable()
-              .scaledToFit()
+            DraggablePhotoView(image: images[index])
           }
         }
         .padding(15)
@@ -24,7 +22,42 @@ struct PhotoAreaView: View {
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(Color(.systemBackground))
-    // .border(Color.green, width: 2)
+  }
+}
+
+struct DraggablePhotoView: View {
+  let image: UIImage
+  @State private var offset: CGSize = .zero
+  @State private var isDragging = false
+
+  var rotationAngle: Double {
+    // Rotate more as you drag further left/right
+    Double(offset.width / 15)
+  }
+
+  var body: some View {
+    Image(uiImage: image)
+      .resizable()
+      .scaledToFit()
+      .frame(maxWidth: 350, maxHeight: 500)
+      .cornerRadius(20)
+      .offset(offset)
+      .rotationEffect(.degrees(rotationAngle))
+      .gesture(
+        DragGesture()
+          .onChanged { value in
+            offset = value.translation
+            isDragging = true
+          }
+          .onEnded { _ in
+            // Snap back for now, no removal
+            withAnimation(.spring()) {
+              offset = .zero
+              isDragging = false
+            }
+          }
+      )
+      .animation(.spring(), value: offset)
   }
 }
 
